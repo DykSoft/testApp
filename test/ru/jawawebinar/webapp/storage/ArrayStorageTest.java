543c7b61df9808f3b13a3ffe3601f140ab86a267
DykSoft <dyksoft@gmail.com>
2016-11-03 23:46:53 +0300

    add junit

---
+++ test/ru/jawawebinar/webapp/storage/ArrayStorageTest.java
@@ -0,0 +1,45 @@
+package ru.jawawebinar.webapp.storage;
+
+import static org.junit.Assert.*;
+
+/**
+ * denis
+ * 02.11.2016.
+ */
+public class ArrayStorageTest {
+    @org.junit.Test
+    public void clear() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void save() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void update() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void load() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void delete() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getAllSorted() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void size() throws Exception {
+
+    }
+
+}
