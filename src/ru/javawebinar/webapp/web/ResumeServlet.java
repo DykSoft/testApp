0434974217fea7d96ea7003f5044e4f3ff57731f
dyksoft <dyksoft@gmail.com>
2016-12-11 01:10:15 +0300

    add serverlet

--- src/ru/javawebinar/webapp/web/ResumeServlet.java
+++ src/ru/javawebinar/webapp/web/ResumeServlet.java
@@ -1,16 +1,28 @@
 package ru.javawebinar.webapp.web;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.Writer;
 
 /**
  * Created by kdeni on 10.12.2016.
  */
 public class ResumeServlet extends javax.servlet.http.HttpServlet {
-    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
 
-    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html; charset=UTF-8");
+        Writer w = response.getWriter();
+        String name = request.getParameter("name");
+        w.write("Тест сервелет: привет ");
+        w.close();
+
 
     }
 }
